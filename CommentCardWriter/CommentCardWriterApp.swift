9ea3e39996adc0d9407757b5bcc2637dac9d0306
Aman <gogna.a@etoncollege.org.uk>
2022-02-04 12:48:31 +0000

    Initial Commit

---
+++ CommentCardWriter/CommentCardWriterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CommentCardWriterApp.swift
+//  CommentCardWriter
+//
+//  Created by Gogna, Aman (HJRM) on 04/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct CommentCardWriterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
